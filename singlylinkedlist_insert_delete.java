3bfd4c15102d076ef96384b16d34effdd9ddea4e
Naveenkumar200326 <naveenkumar200326@gmail.com>
2025-03-17 10:14:12 +0530

    Create singlylinkedlist_insert_delete.java
    singlylinkedlist
    1.insert at start 
    2.insert at end
    3.delete at start 
    4.delete at end

---
+++ singlylinkedlist_insert_delete.java
@@ -0,0 +1,71 @@
+class Node3{
+    int val;
+    Node next;
+    public Node3(int val)
+    {
+        this.val=val;
+        this.next=null;
+    }
+}
+class singly
+{
+    Node head;
+    public void insertatstart(int val)
+    {
+        Node newnode=new Node(val);
+        newnode.next=head;
+        head=newnode;
+    }
+    public void insertatend(int val)
+    {
+        Node newnode=new Node(val);
+        Node temp=head;
+        while (temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=newnode;
+    }
+    public void deleteatstart()
+    {
+        head=head.next;
+    }
+    public void deleteatend()
+    {
+        Node temp=head;
+        while (temp.next.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=null;
+    }
+    public void display()
+    {
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.print(temp.val+"->");
+            temp=temp.next;
+        }
+        System.out.println("End");
+    }
+}
+
+
+
+
+public class exam_question {
+    public static void main(String[] args) {
+        singly s=new singly();
+        s.insertatstart(10);
+        s.insertatstart(20);
+        s.insertatstart(30);
+        s.insertatend(9);
+        s.insertatend(8);
+        s.display();
+        s.deleteatstart();
+        s.display();
+        s.deleteatend();
+        s.display();
+    }
+}
